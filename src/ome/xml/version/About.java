e7abd474a31bd9e960217fe13d3528b64f8165b5
Curtis Rueden <ctrueden@wisc.edu>
2012-08-30 11:48:00 -0500

    Initial version of OME document versioning library

---
+++ src/ome/xml/version/About.java
@@ -0,0 +1,80 @@
+/*
+ * #%L
+ * OME-XML library extension for document versioning.
+ * %%
+ * Copyright (C) 2012 Open Microscopy Environment:
+ *   - Board of Regents of the University of Wisconsin-Madison
+ *   - Glencoe Software, Inc.
+ *   - University of Dundee
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package ome.xml.version;
+
+/**
+ * Displays a small information dialog about the project.
+ *
+ * @author Curtis Rueden
+ */
+public final class About {
+
+  // -- Constants --
+
+  /** URL for OME-XML versioning web page. */
+  public static final String URL_OME_VERSIONING =
+    "http://www.openmicroscopy.org/" +
+    "site/support/file-formats/ome-xml-versioning";
+
+  // -- Static utility methods --
+
+  public static String about() {
+    StringBuilder sb = new StringBuilder();
+    String nl = System.getProperty("line.separator");
+    sb.append("OME-XML versioning extension" + nl);
+    sb.append(nl);
+    sb.append("Copyright (C) 2012 Open Microscopy Environment:" + nl);
+    sb.append("  - Board of Regents " +
+      "of the University of Wisconsin-Madison" + nl);
+    sb.append("  - Glencoe Software, Inc." + nl);
+    sb.append("  - University of Dundee" + nl);
+    sb.append(nl);
+    sb.append(URL_OME_VERSIONING + nl);
+    sb.append(nl);
+    sb.append("Authors: Kristin Briney, Curtis Rueden" + nl);
+    sb.append("Advisors: Roger Leigh, Josh Moore, Andrew Patterson" + nl);
+    return sb.toString();
+  }
+
+  // -- Main method --
+
+  public static void main(String[] args) {
+    System.out.println(about());
+    System.exit(0);
+  }
+
+}
